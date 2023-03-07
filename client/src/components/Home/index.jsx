f99962179798de82695db6c26044adbe300cd905
ernestovv85 <ernestovv85@gmail.com>
2023-03-07 16:06:37 -0600

    Add cards & searchbar

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,20 +1,39 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { getCountries } from "../../redux/countryActions";
-import "./index.css"
+import Card from "../Card";
+import Searchbar from "../Searchbar";
+import "./index.css";
 
 export default function Home() {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const { countries } = useSelector((state) => state.countries);
 
   useEffect(() => {
-    dispatch(getCountries())
-  },[])
+    dispatch(getCountries());
+  }, [dispatch]);
 
   return (
     <div className="container">
-      <header className="bar"></header>
-      <section>
-      <h1>Home</h1>
+      <header className="bar">
+        <Searchbar />
+        <Link to={"/create"} className="link-crear">Crear Actividad</Link>
+      </header>
+      <section className="cards">
+        {countries.map((country) => {
+          return (
+            <div key={country.id}>
+            <Link type="button" to={`/home/${country.id}`} className="linkId">
+              <Card
+                flag={country.flag}
+                name={country.name}
+                continents={country.continents}
+              />
+              </Link>
+            </div>
+          );
+        })}
       </section>
     </div>
   );

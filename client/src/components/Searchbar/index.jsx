6c953302df4cef356876415f66a473c1b91815a4
ernestovv85 <ernestovv85@gmail.com>
2023-03-08 12:38:50 -0600

    Add functionality at component

--- client/src/components/Searchbar/index.jsx
+++ client/src/components/Searchbar/index.jsx
@@ -1,12 +1,38 @@
-import React from "react";
-import lupa from "../../assets/lupa.jpg"
-import "./index.css"
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import lupa from "../../assets/lupa.jpg";
+import { getCountriesByName } from "../../redux/countryActions";
+import "./index.css";
 
 export default function Searchbar() {
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+
+  function handleInputChange(event) {
+    event.preventDefault();
+    setName(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    dispatch(getCountriesByName(name));
+    setName("");
+  }
+
   return (
     <div className="container-searchbar">
-      <input type="text" placeholder="Nombre del país" className="input"/>
-      <button className="btnSearch" ><img src={lupa} alt="buscar" className="img-lupa"/></button>
+      <input
+        type="text"
+        placeholder="Nombre del país"
+        className="input"
+        onChange={(event) => handleInputChange(event)}
+      />
+      <button
+        className="btnSearch"
+        type="submit"
+        onClick={(event) => handleSubmit(event)}
+      >
+        <img src={lupa} alt="buscar" className="img-lupa" />
+      </button>
     </div>
-  )
+  );
 }

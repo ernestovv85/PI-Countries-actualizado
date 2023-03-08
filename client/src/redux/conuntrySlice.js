06fb8eb2a0c8f6428a1c38b31b651582c6c1c163
ernestovv85 <ernestovv85@gmail.com>
2023-03-08 12:35:29 -0600

    Add search country by name

--- client/src/redux/conuntrySlice.js
+++ client/src/redux/conuntrySlice.js
@@ -10,9 +10,12 @@ export const countrySlice = createSlice({
     getAllCountries: (state, action) => {
       state.countries = action.payload;
     },
+    countriesByName: (state, action) => {
+      state.countries = action.payload
+    },
   },
 });
 
-export const { getAllCountries } = countrySlice.actions;
+export const { getAllCountries, countriesByName } = countrySlice.actions;
 
 export default countrySlice.reducer;

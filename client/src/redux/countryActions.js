5e3cbc9f256937729cb8eb009abc09305a24551d
ernestovv85 <ernestovv85@gmail.com>
2023-03-08 12:18:59 -0600

    Add search functionality

--- client/src/redux/countryActions.js
+++ client/src/redux/countryActions.js
@@ -1,9 +1,16 @@
 import axios from "axios";
-import { getAllCountries } from "./conuntrySlice";
+import { getAllCountries, countriesByName } from "./conuntrySlice";
 
 export function getCountries() {
   return async function (dispatch) {
     let json = await axios.get("http://localhost:3001/countries");
     return dispatch(getAllCountries(json.data));
+  };
+}
+
+export function getCountriesByName(name) {
+  return async function (dispatch) {
+    let json = await axios.get(`http://localhost:3001/countries?name=${name}`);
+    return dispatch(countriesByName(json.data));
   };
 }

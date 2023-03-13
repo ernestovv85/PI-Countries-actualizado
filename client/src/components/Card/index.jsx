78dc7c214ddcf001345ad62ef772d2d954914769
ernestovv85 <ernestovv85@gmail.com>
2023-03-13 16:30:32 -0600

    Add Cards & styles

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -5,7 +5,7 @@ export default function Card({name, flag, continents}) {
   return(
     <div className="cardContainer">
       <img src={flag} alt={name} className="flag"/>
-      <h2>{name}</h2>
+      <h3>{name}</h3>
       <h4>Continente: {continents}</h4>
     </div>
   )
